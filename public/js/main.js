63dc3e55c2353d6315c23acbe635609a60a0c34c
Marcelo Martins <m.a.martins@live.com>
2017-07-29 18:07:26 -0300

    Organizaçao do codigo e finalizaçao do projeto.

--- public/js/main.js
+++ public/js/main.js
@@ -78,39 +78,3 @@ function reiniciaJogo() {
     campo.removeClass("borda-verde");
 }
 
-function inserePlacar(){
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "marcelo";
-    var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#' class='botao-remover'><i class='small material-icons'>delete</i></a>" ;
-    var linha = novaLinha(usuario, numPalavras);
-
-    linha.find(".botao-remover").click(removeLinha);
-
-    corpoTabela.prepend(linha);
-}
-
-function novaLinha(usuario,palavras){
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").attr("href","#").addClass("botao-remover");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    link.append(icone);
-
-    colunaRemover.append(link);
-
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
-function removeLinha(event) {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
