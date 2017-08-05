cb4213b5dc084c82453584bb34edce5a37e37786
Marcelo Martins <m.a.martins@live.com>
2017-08-05 19:03:16 -0300

    Buscando dados com Ajax

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,12 +1,18 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
+    $("#spinner").toggle();
+
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
         .fail(function () {
             $("#erro").toggle();
             setTimeout(function () {
                 $("#erro").toggle();
             }, 1500)
+        })
+        .always(function () {
+            $("#spinner").toggle();
         });
 }
 
@@ -17,4 +23,32 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+    $("#spinner").toggle();
+
+    var fraseId = $("#frase-id").val();
+    var dados = { id: fraseId };
+
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+        .fail(function () {
+            $("#erro").toggle();
+            setTimeout(function () {
+                $("#erro").toggle();
+            }, 2000);
+        })
+        .always(function () {
+            $("#spinner").toggle();
+        });
+}
+
+function trocaFrase(data) {
+    console.log(data);
+
+    var frase = $(".frase");
+
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
